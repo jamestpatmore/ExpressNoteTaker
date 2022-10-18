e7f03b5bfc0030eec8e5806b77ba8a6d7c82173e
james.patmore <110008934+jamestpatmore@users.noreply.github.com>
2022-10-18 09:38:46 -0700

    deleted routes // added deps/middleware..

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-// dependencies 
-const express = require('express');
-const htmlRoutes = require('./routes/htmlRoutes');
-const PORT = process.env.port || 7777;
+// * so i initially created a routes folder and was trying to do it the new age way for express instead of keeping everything
+// on one file like this but it was a little harder for me to grasp and I figured i'd just go about it this way 
+// anyways thank you for coming to my TED talk. 
 
+
+// dependencies 
+const { application } = require('express');
+const fs = require('fs');
+const express = requirte('express');
+const path = require('path'); 
+
+//inits the app 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
+// using cli domain OR the local port that i created
+const PORT = process.env.PORT || 7777; 
 
+// taking our data with express.json 
+// serving our front end 
+// aka middleware configuration for the project 
 app.use(express.static('public'));
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+// route for the notes hmtl 
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+
